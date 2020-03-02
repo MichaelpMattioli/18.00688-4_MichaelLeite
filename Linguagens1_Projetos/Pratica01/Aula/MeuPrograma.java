0c941a44ea191c519e27a4d143e9c75e8b2bf8d1
BRUNO  YUJI OSHIRO <18.01771-0@maua.br>
2020-03-02 11:29:32 -0300

    Arquivos criados

---
+++ Linguagens1_Projetos/Pratica01/Aula/MeuPrograma.java
@@ -0,0 +1,9 @@
+class MeuPrograma {
+    public static void main (String[] args){
+
+        // Mensagem de saida do sistema
+        System.out.println("Minha primeira aplicação Java!!");
+        System.out.println("Ola Mundo.");
+        System.out.println("Estou programando no modo Conan!");
+    }
+}
