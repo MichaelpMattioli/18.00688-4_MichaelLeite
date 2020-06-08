8aa623775b96de5f507518e3eedb287931279660
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-08 10:36:48 -0300

    Metodos iniciais do BigBrothers

--- Linguagens1_Projetos/Atividade_08-06/src/br/maua/models/BigBrothers.java
+++ Linguagens1_Projetos/Atividade_08-06/src/br/maua/models/BigBrothers.java
@@ -1,5 +1,22 @@
 package br.maua.models;
 
-public class BigBrothers {
+import br.maua.enums.Cargos;
 
+public class BigBrothers extends Membros{
+
+    public BigBrothers(String usuario, Cargos cargos) {
+        super(usuario, cargos);
+    }
+
+    private String assinaturaTeddyBear = "Sempre ajudando as pessoas membros ou não S2!";
+    private String assinaturaEvilBear = "...";
+
+    private void cadastrarMembro( Membros membros ){
+        
+    }
+
+    @Override
+    public String PostarMensagem(String mensagem) {
+        return null;
+    }
 }
--- Linguagens1_Projetos/Atividade_08-06/src/br/maua/models/Membros.java
+++ Linguagens1_Projetos/Atividade_08-06/src/br/maua/models/Membros.java
@@ -1,6 +1,22 @@
 package br.maua.models;
 
-public abstract class Membros {
+import br.maua.enums.Cargos;
+import br.maua.interfaces.PostarMensagem;
+
+public abstract class Membros implements PostarMensagem {
     private String usuario;
-    private 
+    private Cargos cargos;
+
+    public Membros(String usuario, Cargos cargos) {
+        this.usuario = usuario;
+        this.cargos = cargos;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public Cargos getCargos() {
+        return cargos;
+    }
 }
