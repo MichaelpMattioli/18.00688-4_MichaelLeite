3428ad5bd94a7bbbc1cd85e61b8a43794c614919
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-24 17:58:42 -0300

    caracteristicas da classe pizzas

--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Model/Pizzas.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Model/Pizzas.java
@@ -1,4 +1,13 @@
 package br.maua.Model;
 
 public class Pizzas {
+    private String nome;
+    private String Descricao;
+    private double valor;
+
+    public Pizzas(String nome, String descricao, double valor) {
+        this.nome = nome;
+        Descricao = descricao;
+        this.valor = valor;
+    }
 }
