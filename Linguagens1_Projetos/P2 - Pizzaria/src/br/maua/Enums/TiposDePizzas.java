84cb9ef70db24f536fd44c22bc620f9d2c16b453
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-24 19:35:24 -0300

    Refatoração do enum tiposDePizzas
    colocar valores nas pizzas dentro do enum, para assim facilitar na procura do valor de cada pizza e facilitar as contas do valor do pedido

--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Enums/TiposDePizzas.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Enums/TiposDePizzas.java
@@ -1,5 +1,14 @@
 package br.maua.Enums;
 
 public enum TiposDePizzas {
-    MARGUERITA, CALABRESA, PORTUGUESA
+    MARGUERITA(35.90), CALABRESA(35.90), PORTUGUESA(39.90);
+
+    private double valorDaPizza;
+    TiposDePizzas(double valor) {
+        valorDaPizza = valor;
+    }
+
+    public double getValorDaPizza() {
+        return valorDaPizza;
+    }
 }
