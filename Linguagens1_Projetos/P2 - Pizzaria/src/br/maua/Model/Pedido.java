468e6cb9ffd5cc3bc2625efefef01255d9c44b78
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-24 16:56:37 -0300

    Colocando as caracteristicas, construtor, getter e setter para a classe Pedido

--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Model/Pedido.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Model/Pedido.java
@@ -1,4 +1,47 @@
 package br.maua.Model;
 
+import br.maua.Enums.EstadoDoPedido;
+
 public class Pedido {
+    private int ID;
+    private String Descricao;
+    private double valor;
+    private String formaDePagamento;
+    private EstadoDoPedido estado;
+
+    public Pedido(int ID, String descricao, double valor, String formaDePagamento, String estado) {
+        this.ID = ID;
+        Descricao = descricao;
+        this.valor = valor;
+        this.formaDePagamento = formaDePagamento;
+        this.estado = EstadoDoPedido.REALIZADO;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getFormaDePagamento() {
+        return formaDePagamento;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setFormaDePagamento(String formaDePagamento) {
+        this.formaDePagamento = formaDePagamento;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
