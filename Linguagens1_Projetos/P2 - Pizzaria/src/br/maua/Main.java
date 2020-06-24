540ab8717b6656882712d19f10809bd0b9a64eaa
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-24 17:42:18 -0300

    Teste para saber se a classe usuario está okay

--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Main.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Main.java
@@ -1,11 +1,18 @@
 package br.maua;
 
 import br.maua.Model.SistemaPrincipal;
+import br.maua.Model.Usuario;
 
 public class Main {
 
     public static void main(String[] args) {
         SistemaPrincipal sistemaPrincipal = new SistemaPrincipal();
         sistemaPrincipal.Menu();
+
+        Usuario usuario1 = new Usuario("Ronaldo","RonaldoBrilhaMuitoNoCurinthians@bol.com", "123456");
+        System.out.println(usuario1.getEmail());
+        System.out.println(usuario1.getNome());
+        System.out.println(usuario1.getSenha());
+
     }
 }
