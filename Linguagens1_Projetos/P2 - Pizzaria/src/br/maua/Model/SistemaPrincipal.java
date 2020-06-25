72ce4bbd0194eb2a6e440550366d54a03616fd7a
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-25 03:26:06 -0300

    Criação da lista de pedidos no sistema Principal

--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Model/SistemaPrincipal.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Model/SistemaPrincipal.java
@@ -1,21 +1,27 @@
 package br.maua.Model;
 
+import br.maua.Enums.TiposDePizzas;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SistemaPrincipal{
+    Pedidos pedidos = new Pedidos();
+    List<Pedidos> listaDosPedidos = new ArrayList<>();
 
     public void Menu(){
+        System.out.println("------------||------------");
         System.out.println("Pizzaria o Rato que Ri");
         System.out.println("1 - Nova venda");
         System.out.println("2 - Verificar pedidos");
         System.out.println("3 - Alterar pedidos");
         System.out.println("0 - Sair");
+        System.out.println("------------||------------");
     }
 
     public void Sistema(){
         Usuario admin = new Usuario("Rato","pizzariadoratoqueri@gmail.com", "123456");
-        Pedidos pedidos = new Pedidos();
         Scanner scanner = new Scanner(System.in);
 
         int escolha = 0;
@@ -24,18 +30,27 @@ public class SistemaPrincipal{
             escolha = scanner.nextInt();
             switch (escolha) {
                 case 1:
-                    pedidos.criarPedido();
+                    pedidos = pedidos.criarPedido();
+                    listaDosPedidos.add(pedidos);
                     break;
                 case 2:
+                    mostraPedidos(listaDosPedidos);
                     break;
                 case 3:
+                    System.out.print("Informe o ID do pedido que queira alterar: ");
+                    int iD = scanner.nextInt();
+                    pedidos.mudarPedido(iD);
                     break;
                 case 0:
                     System.out.println("------------| Saiu do sistema |------------");
                     break;
             }
-    }while (escolha != 0);
-}
+        }while (escolha != 0);
+    }
 
-
+    private void  mostraPedidos(List<Pedidos> listaDosPedidos){
+        for (Pedidos pedido : listaDosPedidos) {
+            System.out.println(pedido);
+        }
+    }
 }
