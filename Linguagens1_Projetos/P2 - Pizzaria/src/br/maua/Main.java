939960e7d09339b7ce9828b87573f155438bd010
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-25 06:54:19 -0300

    Documentações

--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Main.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Main.java
@@ -1,6 +1,15 @@
 package br.maua;
 
-import br.maua.Model.*;
+import br.maua.Model.SistemaPrincipal;
+
+/**
+ * Classe ponto de entrada da aplicação base.
+ * Ela é responsável por iniciar a aplicação, criando as instâncias dos objetos.
+ * Por meio dela, a aplicação é executada.
+ * @author Michael Pedroza Mattioli Leite
+ * @since 25/06/2020
+ * @version 1.0
+ */
 
 public class Main {
 
--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Model/SistemaPrincipal.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Model/SistemaPrincipal.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class SistemaPrincipal{
-    List<Pedidos> listaDosPedidos = new ArrayList<>();
 
     private void Menu(){
         System.out.println("------------||------------");
@@ -17,48 +16,50 @@ public class SistemaPrincipal{
         System.out.print("------------||------------");
     }
 
-    public void Sistema(){
-        Usuario admin = new Usuario("Rato","pizzariadoratoqueri@gmail.com", "123456");
+    public void Sistema() {
+        Usuario admin = new Usuario("Rato", "pizzariadoratoqueri@gmail.com", "123456");
         Scanner scanner = new Scanner(System.in);
         Pedidos pedidos = new Pedidos();
+        List<Pedidos> listaDosPedidos = new ArrayList<>();
 
         int escolha = 0;
 
-        do {Menu();
+        do {
+            Menu();
             String senha = "0";
             escolha = scanner.nextInt();
             switch (escolha) {
                 case 1:
-                    System.out.println("Digite sua senha:");
+                    System.out.print("Digite sua senha: ");
                     senha = scanner.next();
-                    if( admin.autenticacaoDoUsuario(senha )) {
+                    if (admin.autenticacaoDoUsuario(senha)) {
                         pedidos = pedidos.criarPedido();
-                        if(pedidos.getDescricao().equals("null") || pedidos.getDescricao().equals(""))
+                        if (pedidos.getDescricao().equals("null") || pedidos.getDescricao().equals(""))
                             break;
                         else
-                            listaDosPedidos.add(pedidos);}
+                            listaDosPedidos.add(pedidos);
+                    }
                     break;
                 case 2:
-                    mostraPedidos(listaDosPedidos);
+                    for (Pedidos pedido : listaDosPedidos)
+                        System.out.println(pedido);
                     break;
                 case 3:
-                    System.out.println("Digite sua senha");
+                    System.out.print("Digite sua senha: ");
                     senha = scanner.next();
-                    if( admin.autenticacaoDoUsuario(senha )) {
+                    if (admin.autenticacaoDoUsuario(senha)) {
                         System.out.print("Informe o ID do pedido que queira alterar: ");
                         String iD = scanner.next();
-                        pedidos.mudarPedido(iD,listaDosPedidos);}
+                        pedidos.mudarPedido(iD, listaDosPedidos);
+                    }
                     break;
                 case 0:
                     System.out.println("------------| Saiu do sistema |------------");
                     break;
+
             }
-        }while (escolha != 0);
+        } while (escolha != 0) ;
+
     }
 
-    private void  mostraPedidos(List<Pedidos> listaDosPedidos){
-        for (Pedidos pedido : listaDosPedidos) {
-            System.out.println(pedido);
-        }
-    }
 }
