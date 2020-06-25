0e149eb6fa0512620e6ecc4fe27629327086bced
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-25 01:37:31 -0300

    Mudança no toString de Pizzas

--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Model/Pizzas.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Model/Pizzas.java
@@ -24,8 +24,6 @@ public class Pizzas {
 
     @Override
     public String toString() {
-        return "Pizzas{" +
-                "nome=" + nome +
-                '}';
+        return " " + nome + " ";
     }
 }
