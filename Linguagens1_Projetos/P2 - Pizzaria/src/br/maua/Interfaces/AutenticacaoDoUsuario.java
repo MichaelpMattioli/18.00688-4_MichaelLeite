a5be66d34e1fdc13b3196714a8c913f43979b77a
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-25 06:30:34 -0300

    Refatoração da interface autenticacaoDoUsuario
    Refatoração para que a interface tenha apenas 1 parametro no seu método, assim facilita a visualização e a reutilização da mesma

--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Interfaces/AutenticacaoDoUsuario.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Interfaces/AutenticacaoDoUsuario.java
@@ -1,9 +1,5 @@
 package br.maua.Interfaces;
 
-import br.maua.Model.Usuario;
-
-import java.util.List;
-
 public interface AutenticacaoDoUsuario {
-    boolean autenticacaoDoUsuario(Usuario user, String senha);
+    boolean autenticacaoDoUsuario(String senha);
 }
