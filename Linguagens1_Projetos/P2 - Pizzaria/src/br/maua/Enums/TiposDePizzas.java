7f01bc9b26d3a0525b1a0328c1fcc67de1ae330a
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-25 05:31:47 -0300

    Refatoração + Documentação
    A refatoração ocorreu para ver se ao adicionar mais um enum, o sistema ainda continua consistente, o que mostrou que sim, ele não teve nenhum problemas.

--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Enums/TiposDePizzas.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Enums/TiposDePizzas.java
@@ -1,12 +1,29 @@
 package br.maua.Enums;
 
+/**
+ * Classe responsavel enumerar os tipos de pizzas e seus valores.
+ * @author Michael Pedroza Mattioli Leite
+ * @since 24/06/2020
+ * @version 1.0
+ */
+
 public enum TiposDePizzas {
-    MARGUERITA(35.90), CALABRESA(35.90), PORTUGUESA(39.90);
+    MARGUERITA(35.90), CALABRESA(35.90), PORTUGUESA(39.90),FRANGO_COM_CAPUTIRY(39.90);
 
     private double valorDaPizza;
+
+    /**
+     * Construtor responsavel por indicar o valor de cada enumeracao.
+     * @param valor
+     */
     TiposDePizzas(double valor) {
         valorDaPizza = valor;
     }
+
+    /**
+     * Método responsavel por retornar o valor contido na enumeracao.
+     * @return valor contido na enumeracao
+     */
 
     public double getValorDaPizza() {
         return valorDaPizza;
