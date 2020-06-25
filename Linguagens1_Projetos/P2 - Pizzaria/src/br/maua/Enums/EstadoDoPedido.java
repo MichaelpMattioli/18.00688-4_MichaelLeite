fd4348e74b720fe31d82cdbd7d0193ccdf668b16
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-25 06:34:11 -0300

    Documentação dos enums EstadoDoPedido e FormaDePagamento

--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Enums/EstadoDoPedido.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Enums/EstadoDoPedido.java
@@ -1,5 +1,12 @@
 package br.maua.Enums;
 
+/**
+ * Enum responsavel enumerar os estados do pedido.
+ * @author Michael Pedroza Mattioli Leite
+ * @since 25/06/2020
+ * @version 1.0
+ */
+
 public enum EstadoDoPedido {
     REALIZADO, PREPARACAO, SAIU_PARA_ENTREGAR, ENTREGUE, DEVOLVIDO
 }
--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Enums/FormaDePagamento.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Enums/FormaDePagamento.java
@@ -1,5 +1,12 @@
 package br.maua.Enums;
 
+/**
+ * Enum responsavel enumerar os tipos de formas de pagamentos.
+ * @author Michael Pedroza Mattioli Leite
+ * @since 25/06/2020
+ * @version 1.0
+ */
+
 public enum FormaDePagamento {
     DINHEIRO, DEBITO, CREDITO, VALE_ALIMENTACAO, VALE_REFEICAO
 }
