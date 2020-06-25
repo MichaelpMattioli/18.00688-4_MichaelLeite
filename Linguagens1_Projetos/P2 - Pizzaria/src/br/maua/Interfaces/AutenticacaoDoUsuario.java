96835fcaa7d6043491ab5c8bf70926cb1a07626b
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-25 04:18:39 -0300

    Refatoração do Autenticação do usuario
    a Autenticação é realizada apenas pela senha

--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Interfaces/AutenticacaoDoUsuario.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Interfaces/AutenticacaoDoUsuario.java
@@ -5,5 +5,5 @@ import br.maua.Model.Usuario;
 import java.util.List;
 
 public interface AutenticacaoDoUsuario {
-    List AutenticacaoDoUsuario(List<Usuario> listaDeUsuarios) ;
+    boolean autenticacaoDoUsuario(String senha);
 }
