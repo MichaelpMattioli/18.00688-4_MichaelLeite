7b50653e3929c832f408bccc584a8fe5664dd499
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-06-25 05:29:13 -0300

    Refatoração da autenticacao
    A autenticação necessita de um user do tipo Usuario, para assim comparar com a senha que o usuario irá digitar.

--- Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Interfaces/AutenticacaoDoUsuario.java
+++ Linguagens1_Projetos/P2 - Pizzaria/src/br/maua/Interfaces/AutenticacaoDoUsuario.java
@@ -5,5 +5,5 @@ import br.maua.Model.Usuario;
 import java.util.List;
 
 public interface AutenticacaoDoUsuario {
-    boolean autenticacaoDoUsuario(String senha);
+    boolean autenticacaoDoUsuario(Usuario user, String senha);
 }
