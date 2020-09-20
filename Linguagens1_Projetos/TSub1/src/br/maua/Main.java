e83322b07271690531b308114922026d93ff2b30
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-09-20 19:57:24 -0300

    Base do projeto
    Criação do projeto com o banco de dados e configurado para o sql

---
+++ Linguagens1_Projetos/TSub1/src/br/maua/Main.java
@@ -0,0 +1,11 @@
+// Michael Pedroza Mattioli leite RA: 18.00688-4
+// José Guilherme Martins dos Santos RA: 18.00135-0
+
+package br.maua;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
