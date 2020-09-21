a2ec5ad8b0fb53f5f1ec22104e1d90db6e6d987c
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-09-21 02:01:45 -0300

    Modificações na Main para rodar o FX

--- Linguagens1_Projetos/TSub1/src/br/maua/Main.java
+++ Linguagens1_Projetos/TSub1/src/br/maua/Main.java
@@ -3,13 +3,23 @@
 
 package br.maua;
 
-import br.maua.teste.AplicacaoDAO;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 
     public static void main(String[] args) {
-        AplicacaoDAO app = new AplicacaoDAO();
-        app.run();
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ViewInterface.fxml"));
+        primaryStage.setTitle("Pokedex");
+        primaryStage.setScene(new Scene(root, 1280,1024));
+        primaryStage.show();
     }
 }
