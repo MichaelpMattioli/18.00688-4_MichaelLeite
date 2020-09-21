f8777a97d29349eeb8768d96b34b3f89e6471d17
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-09-21 03:27:06 -0300

    Criação da lista de cartas pokemon
    a lista de cartas irá gerenciar a carta que está sendo exibida atualmente

---
+++ Linguagens1_Projetos/TSub1/src/br/maua/model/ListaCartasPokemon.java
@@ -0,0 +1,33 @@
+package br.maua.model;
+
+import java.util.List;
+
+public class ListaCartasPokemon {
+    private List<CartasPokemon> pokemonList;
+    private int pokemonAtual;
+
+    public ListaCartasPokemon(List<CartasPokemon> pokemonList) {
+        this.pokemonList = pokemonList;
+        this.pokemonAtual = 0;
+    }
+
+    public final CartasPokemon getCartaAtual(){
+        return pokemonList.get(pokemonAtual);
+    }
+
+    public boolean switchToPreviousCard(){
+        if(pokemonAtual > 0){
+            pokemonAtual--;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean switchToNextCard(){
+        if(pokemonAtual < pokemonList.size() - 1){
+            pokemonAtual++;
+            return true;
+        }
+        return false;
+    }
+}
