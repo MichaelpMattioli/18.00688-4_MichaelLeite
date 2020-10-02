440d23e563afa2ba298f43c25a6f736135bed0ca
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-02 20:41:56 -0300

    Criação das variaveis, construtor, getters e toString

--- Linguagens1_Projetos/T3/src/br/maua/models/Manga.java
+++ Linguagens1_Projetos/T3/src/br/maua/models/Manga.java
@@ -1,4 +1,61 @@
 package br.maua.models;
 
 public class Manga {
+    private String nome;
+    private String sinopse;
+    private int quantidadeCapitulos;
+    private int quantidadeVolumes;
+    private String tipo;
+    private float nota;
+    private String urlPoster;
+
+    public Manga(String nome, String sinopse, int quantidadeCapitulos, int quantidadeVolumes, String tipo, float nota, String urlPoster) {
+        this.nome = nome;
+        this.sinopse = sinopse;
+        this.quantidadeCapitulos = quantidadeCapitulos;
+        this.quantidadeVolumes = quantidadeVolumes;
+        this.tipo = tipo;
+        this.nota = nota;
+        this.urlPoster = urlPoster;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public int getQuantidadeCapitulos() {
+        return quantidadeCapitulos;
+    }
+
+    public int getQuantidadeVolumes() {
+        return quantidadeVolumes;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public String getUrlPoster() {
+        return urlPoster;
+    }
+
+    @Override
+    public String toString() {
+        return "Manga - " +
+                "nome: " + nome +
+                ", sinopse: " + sinopse +
+                ", quantidadeCapitulos: " + quantidadeCapitulos +
+                ", quantidadeVolumes: " + quantidadeVolumes +
+                ", tipo: " + tipo +
+                ", nota: " + nota +
+                ", urlPoster: " + urlPoster;
+    }
 }
