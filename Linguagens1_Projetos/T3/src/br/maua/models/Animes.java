4bea81ffe81ca7bb5bcb491d6352463a7a4b0766
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-02 20:36:36 -0300

    Implementação das variavers, construtor, getter e toString

--- Linguagens1_Projetos/T3/src/br/maua/models/Animes.java
+++ Linguagens1_Projetos/T3/src/br/maua/models/Animes.java
@@ -1,4 +1,40 @@
 package br.maua.models;
 
+
 public class Animes {
+    private String nome;
+    private String sinopse;
+    private int quantidadeEpisodios;
+    private float nota;
+
+    public Animes(String nome, String sinopse, int quantidadeEpisodios, float nota) {
+        this.nome = nome;
+        this.sinopse = sinopse;
+        this.quantidadeEpisodios = quantidadeEpisodios;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public int getQuantidadeEpisodios() {
+        return quantidadeEpisodios;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    @Override
+    public String toString() {
+        return "nome: " + nome  +
+                ", sinopse: " + sinopse +
+                ", quantidadeEpisodios: " + quantidadeEpisodios +
+                ", nota: " + nota ;
+    }
 }
