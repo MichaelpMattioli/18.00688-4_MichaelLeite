431492eb7b0b19e6212857def5cb4973d86de1ec
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-03 19:13:38 -0300

    Documentação + retirada de metodos nao utilizados

--- Linguagens1_Projetos/T3/src/br/maua/models/Animes.java
+++ Linguagens1_Projetos/T3/src/br/maua/models/Animes.java
@@ -1,6 +1,11 @@
 package br.maua.models;
 
-
+/**
+ * * Classe responsavel por modelar os dados de cada Anime.
+ * @author José Guilherme Martins dos Santos - josegms2000@gmail.com    Michael Pedroza Mattioli Leite - michael.pmattioli@gmail.com
+ * @since 02/10/2020
+ * @version 1.0
+ * */
 public class Animes {
     private String nome;
     private String sinopse;
@@ -8,6 +13,14 @@ public class Animes {
     private Float nota;
     private String urlPoster;
 
+    /**
+     * Construtor de Animes
+     * @param nome nome atribuido ao anime.
+     * @param sinopse sinopse atribuida ao anime.
+     * @param quantidadeEpisodios quantidade de episodios atribuido ao anime.
+     * @param nota nota atribuida ao anime.
+     * @param urlPoster url atribuida ao anime.
+     */
     public Animes(String nome, String sinopse, int quantidadeEpisodios, float nota, String urlPoster) {
         this.nome = nome;
         this.sinopse = sinopse;
@@ -16,34 +29,50 @@ public class Animes {
         this.urlPoster = urlPoster;
     }
 
-    public Animes(String nome) {
-        this.nome = nome;
-        this.sinopse = null;
-        this.quantidadeEpisodios = null;
-        this.nota = null;
-        this.urlPoster = null;
-    }
-
+    /**
+     * Função para obter o nome do anime.
+     * @return retorna o nome do anime.
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Função para obter a Sinopse do anime.
+     * @return retorna a sinopse do anime.
+     */
     public String getSinopse() {
         return sinopse;
     }
 
+    /**
+     * Função para obter a quantidade de episodios.
+     * @return retorna a quantidade de episodios do anime.
+     */
     public int getQuantidadeEpisodios() {
         return quantidadeEpisodios;
     }
 
+    /**
+     * Função para obter a nota do anime.
+     * @return retorna a nota atribuida ao anime.
+     */
     public float getNota() {
         return nota;
     }
 
+    /**
+     * Função para obter a Url do anime.
+     * @return retorna a URL atribuida ao anime.
+     */
     public String getUrlPoster() {
         return urlPoster;
     }
 
+    /**
+     * Função para formatar em formato de texto as informações do anime.
+     * @return retorna as informações do anime de forma formata para texto.
+     */
     @Override
     public String toString() {
         return "Anime - " +
@@ -51,6 +80,6 @@ public class Animes {
                 ", sinopse: " + sinopse +
                 ", quantidadeEpisodios: " + quantidadeEpisodios +
                 ", nota: " + nota +
-                ", urlPoster" + urlPoster;
+                ", urlPoster: " + urlPoster;
     }
 }
