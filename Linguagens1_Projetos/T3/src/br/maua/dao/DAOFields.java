028bcfbd2feaace7421ffb4a5a08f6b30219e736
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-03 19:12:21 -0300

    Retirada dos metodos nao utilizados

--- Linguagens1_Projetos/T3/src/br/maua/dao/DAOFields.java
+++ Linguagens1_Projetos/T3/src/br/maua/dao/DAOFields.java
@@ -1,9 +1,14 @@
 package br.maua.dao;
 
+/**
+ * * Interface responsável por implementar os métodos para interagir com o SQL.
+ * @author José Guilherme Martins dos Santos - josegms2000@gmail.com    Michael Pedroza Mattioli Leite - michael.pmattioli@gmail.com
+ * @since 02/10/2020
+ * @version 1.0
+ * */
+
 public interface DAOFields {
     String getTableName();
-    String getDeleteString(String table);
-    String getUpdateString(String table);
     String getInsertString(String table);
     String getSelectAllString(String table);
     String getSelectConditionalString(String table);
