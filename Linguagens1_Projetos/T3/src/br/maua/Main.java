987a0353c758d1e807e7d868f6facadd2c900d1c
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-03 14:04:47 -0300

    teste na main

--- Linguagens1_Projetos/T3/src/br/maua/Main.java
+++ Linguagens1_Projetos/T3/src/br/maua/Main.java
@@ -4,28 +4,10 @@ package br.maua;
 
 import br.maua.aplicacoes.AplicacaoDAO;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
         AplicacaoDAO aplicacaoDAO = new AplicacaoDAO();
         aplicacaoDAO.run();
-    }
-
-    private static void leituraJava11() throws Exception{
-        HttpClient client = HttpClient.newBuilder().build();
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("https://api.jikan.moe/v3/anime/1"))
-                .build();
-        HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
-        System.out.println("Status Code:" + response.statusCode());
-        System.out.println("Recebidos:");
-        System.out.println(response.body());
     }
 }
