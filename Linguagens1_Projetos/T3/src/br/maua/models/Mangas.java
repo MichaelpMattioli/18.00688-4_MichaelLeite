e039bd79e2f4414462e17365bc175404b8ca3aad
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-03 19:15:20 -0300

    Documentação + retirada de metodo nao utilizado

--- Linguagens1_Projetos/T3/src/br/maua/models/Mangas.java
+++ Linguagens1_Projetos/T3/src/br/maua/models/Mangas.java
@@ -1,5 +1,12 @@
 package br.maua.models;
 
+/**
+ * * Classe responsavel por modelar os dados de cada mangá.
+ * @author José Guilherme Martins dos Santos - josegms2000@gmail.com    Michael Pedroza Mattioli Leite - michael.pmattioli@gmail.com
+ * @since 02/10/2020
+ * @version 1.0
+ * */
+
 public class Mangas {
     private String nome;
     private String sinopse;
@@ -8,6 +15,17 @@ public class Mangas {
     private String tipo;
     private Float nota;
     private String urlPoster;
+
+    /**
+     * Construtor da classe Mangas
+     * @param nome String que apresenta o nome do mangá
+     * @param sinopse String que aparesenta a sinopse do mangá
+     * @param quantidadeCapitulos Integer que apresenta a quantidade de capítulos do mangá.
+     * @param quantidadeVolumes Integer que apresenta a quantidade de volumes do mangá.
+     * @param tipo String que apresenta o tipo de mangá.
+     * @param nota Float que apresenta a nota do mangá.
+     * @param urlPoster String que apresenta o URL do poster no mangá.
+     */
 
     public Mangas(String nome, String sinopse, int quantidadeCapitulos, int quantidadeVolumes, String tipo, float nota, String urlPoster) {
         this.nome = nome;
@@ -19,44 +37,66 @@ public class Mangas {
         this.urlPoster = urlPoster;
     }
 
-    public Mangas(String nome) {
-        this.nome = nome;
-        this.sinopse = null;
-        this.quantidadeCapitulos = null;
-        this.quantidadeVolumes = null;
-        this.tipo = null;
-        this.nota = null;
-        this.urlPoster = null;
-    }
-
+    /**
+     * Função para obter o nome do Mangá.
+     * @return retorna o nome do Mangá.
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Função para obter a sinopse do Mangá.
+     * @return retorna a sinopse do Mangá.
+     */
     public String getSinopse() {
         return sinopse;
     }
 
+    /**
+     * Função para obter a quantidade de capitulos do Mangá.
+     * @return retorna a quantidade de capitulos do Mangá.
+     */
     public int getQuantidadeCapitulos() {
         return quantidadeCapitulos;
     }
 
+    /**
+     * Função para obter a quantidade de volumes do Mangá.
+     * @return retorna a quantidade de volumes do Mangá.
+     */
     public int getQuantidadeVolumes() {
         return quantidadeVolumes;
     }
 
+    /**
+     * Função para obter o tipo do Mangá.
+     * @return retorna o tipo do Mangá.
+     */
     public String getTipo() {
         return tipo;
     }
 
+    /**
+     * Função para obter a nota do Mangá.
+     * @return retorna a nota do Mangá.
+     */
     public float getNota() {
         return nota;
     }
 
+    /**
+     * Função para obter a URL do Mangá.
+     * @return retorna a URL do Mangá.
+     */
     public String getUrlPoster() {
         return urlPoster;
     }
 
+    /**
+     * Função para formatar em formato de texto as informações do Mangá.
+     * @return retorna as informações do Mangá de forma formata para texto.
+     */
     @Override
     public String toString() {
         return "Manga - " +
