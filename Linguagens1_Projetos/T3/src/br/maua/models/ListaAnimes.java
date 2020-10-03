f92121bd7451f7a1f682de817e1fd50bdf23254a
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-02 23:27:06 -0300

    Implementação
    criação de uma lista do tipo animes para deixar sempre atualizado no programa quais são os animes que estão no banco de dados

--- Linguagens1_Projetos/T3/src/br/maua/models/ListaAnimes.java
+++ Linguagens1_Projetos/T3/src/br/maua/models/ListaAnimes.java
@@ -1,4 +1,12 @@
 package br.maua.models;
 
+import java.util.List;
+
 public class ListaAnimes {
+    private List<Animes> animesList;
+
+    public ListaAnimes(List<Animes> animesList) {
+        this.animesList = animesList;
+    }
+    
 }
