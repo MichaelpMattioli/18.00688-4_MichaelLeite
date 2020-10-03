9b1a26c2ba91d40353c17641577be6cf5e289006
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-02 23:59:39 -0300

    Refatorando
    Adição de um construtor que aceita apenas o nome do anime, e deixando nulo os demais parametros

--- Linguagens1_Projetos/T3/src/br/maua/models/Animes.java
+++ Linguagens1_Projetos/T3/src/br/maua/models/Animes.java
@@ -4,8 +4,8 @@ package br.maua.models;
 public class Animes {
     private String nome;
     private String sinopse;
-    private int quantidadeEpisodios;
-    private float nota;
+    private Integer quantidadeEpisodios;
+    private Float nota;
     private String urlPoster;
 
     public Animes(String nome, String sinopse, int quantidadeEpisodios, float nota, String urlPoster) {
@@ -14,6 +14,14 @@ public class Animes {
         this.quantidadeEpisodios = quantidadeEpisodios;
         this.nota = nota;
         this.urlPoster = urlPoster;
+    }
+
+    public Animes(String nome) {
+        this.nome = nome;
+        this.sinopse = null;
+        this.quantidadeEpisodios = null;
+        this.nota = null;
+        this.urlPoster = null;
     }
 
     public String getNome() {
