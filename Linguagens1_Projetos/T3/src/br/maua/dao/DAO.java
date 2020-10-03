90f0dfb701a06d7feb4d4e1419f579d5c7bae310
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-03 19:12:13 -0300

    Retirada dos metodos nao utilizados

--- Linguagens1_Projetos/T3/src/br/maua/dao/DAO.java
+++ Linguagens1_Projetos/T3/src/br/maua/dao/DAO.java
@@ -1,12 +1,14 @@
 package br.maua.dao;
 
-import java.sql.SQLException;
 import java.util.List;
-
+/**
+ * * Interface responsável pela representação do DAO que sera utilizado, são descritos o get, getALL e create.
+ * @author José Guilherme Martins dos Santos - josegms2000@gmail.com    Michael Pedroza Mattioli Leite - michael.pmattioli@gmail.com
+ * @since 19/09/2020
+ * @version 1.0
+ * */
 public interface DAO <T> {
     List<T> get(String condition);
     List<T> getAll();
-    void update(T t);
-    void delete(T t);
     void create(T t);
 }
