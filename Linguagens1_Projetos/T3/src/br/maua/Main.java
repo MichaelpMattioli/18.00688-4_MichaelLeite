4bd803117b7b4c7178fa5b9e0274ed7c1a2f947e
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-03 19:13:15 -0300

    documentação

--- Linguagens1_Projetos/T3/src/br/maua/Main.java
+++ Linguagens1_Projetos/T3/src/br/maua/Main.java
@@ -1,11 +1,18 @@
 //Michael Pedroza Mattioli Leite RA: 18.00688-4
+// José Guilherme Martins dos Santos RA: 18.00135-0
+
+/**
+ * * Classe responsável por incializar o programa.
+ * @author José Guilherme Martins dos Santos - josegms2000@gmail.com    Michael Pedroza Mattioli Leite - michael.pmattioli@gmail.com
+ * @since 02/10/2020
+ * @version 1.0
+ * */
 
 package br.maua;
 
 import br.maua.aplicacoes.AplicacaoDAO;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         AplicacaoDAO aplicacaoDAO = new AplicacaoDAO();
         aplicacaoDAO.run();
