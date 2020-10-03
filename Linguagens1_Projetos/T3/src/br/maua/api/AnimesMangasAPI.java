94b39f21367ce54122e99d294f588651c0448670
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-03 19:11:29 -0300

    java doc da classe API

--- Linguagens1_Projetos/T3/src/br/maua/api/AnimesMangasAPI.java
+++ Linguagens1_Projetos/T3/src/br/maua/api/AnimesMangasAPI.java
@@ -1,16 +1,26 @@
 package br.maua.api;
 
-import br.maua.models.Animes;
 import org.json.JSONObject;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
+/**
+ * * Classe responsavel pela interação com a API do site api.jikan.moe/v3
+ * @author José Guilherme Martins dos Santos - josegms2000@gmail.com    Michael Pedroza Mattioli Leite - michael.pmattioli@gmail.com
+ * @since 02/10/2020
+ * @version 1.0
+ * */
 public class AnimesMangasAPI {
+    /**
+     * Função responsavel por pesquisar um um titulo especifico de algum anime ou mangá.
+     * @param tipo String que deve ser informada se é "anime" ou "manga".
+     * @param nome tring que deve ser informada o titulo.
+     * @return Uma estrutura JSON com os dados que foram requisitados.
+     * @throws Exception lança uma exceção caso nao seja possivel criar o json.
+     */
     public JSONObject formatoJson(String tipo, String nome) throws Exception{
-
         HttpClient client = HttpClient.newBuilder().build();
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
