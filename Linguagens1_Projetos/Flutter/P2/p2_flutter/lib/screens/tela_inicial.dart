2328f3f70334e031c91a8f5b53056c2859185c32
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-11-28 14:56:06 -0300

    Criação do logo retirado no assets

--- Linguagens1_Projetos/Flutter/P2/p2_flutter/lib/screens/tela_inicial.dart
+++ Linguagens1_Projetos/Flutter/P2/p2_flutter/lib/screens/tela_inicial.dart
@@ -6,8 +6,14 @@ class TelaInicial extends StatelessWidget {
   final controlador = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(child: SizedBox(width:200, height: 100, child: Image.asset("assets/Logo/LogoImovel.png")))
+          ],
+        ),
+      ),
     );
   }
 }
