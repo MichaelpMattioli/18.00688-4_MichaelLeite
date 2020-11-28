7680e12f8764c7e85cafb541ce89ed5f853b6c31
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-11-28 13:44:08 -0300

    Criação da primeira tela

--- Linguagens1_Projetos/Flutter/P2/p2_flutter/lib/main.dart
+++ Linguagens1_Projetos/Flutter/P2/p2_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:p2_flutter/screens/tela_inicial.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +11,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TelaInicial(),
     );
   }
 }
---
+++ Linguagens1_Projetos/Flutter/P2/p2_flutter/lib/screens/tela_inicial.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TelaInicial extends StatelessWidget {
+
+  final controlador = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
