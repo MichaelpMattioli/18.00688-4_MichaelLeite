56dcd7963532a88d885d862bb933fb4a671a89eb
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-11-29 05:50:41 -0300

    Refatoração para um ListView e Adição de um Button para google maps
    o ListView evita que tenha um overflow, colocando a fisica de movimentação.
    Já o botão redireciona para uma tela onde apresenta a localização do imovel

--- Linguagens1_Projetos/Flutter/P2/p2_flutter/lib/screens/imovel_infos.dart
+++ Linguagens1_Projetos/Flutter/P2/p2_flutter/lib/screens/imovel_infos.dart
@@ -17,74 +17,86 @@ class ImovelInfos extends StatelessWidget {
         backgroundColor: Colors.blueGrey[100],
         body: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.network(imovel.fachada),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.blueGrey[500],
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Column(
+            Expanded(
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(width: 200, height: 500, child: Image.network(imovel.fachada),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      color: Colors.blueGrey,
+                      child: Row(
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Nome: ${imovel.nome}"),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("Nome: ${imovel.nome}"),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("Preço: ${imovel.planta.preco}"),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(" Dormitorios: ${imovel.planta.dorms}"),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("Vagas: ${imovel.planta.vagas}"),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("Metragem: ${imovel.planta.metragem}"),
+                                ),
+                              ],
+                            ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Preço: ${imovel.planta.preco}"),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(" Dormitorios: ${imovel.planta.dorms}"),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Vagas: ${imovel.planta.vagas}"),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Metragem: ${imovel.planta.metragem}"),
-                          ),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("Rua: ${imovel.rua}"),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("Número: ${imovel.num}"),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("Bairro: ${imovel.bairro}"),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("Cidade: ${imovel.cidade}"),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("CEP: ${imovel.cep}"),
+                                ),
+                              ],
+                            ),
+                          )
                         ],
                       ),
                     ),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Rua: ${imovel.rua}"),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Número: ${imovel.num}"),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Bairro: ${imovel.bairro}"),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Cidade: ${imovel.cidade}"),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("CEP: ${imovel.cep}"),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                  ),
+                  FlatButton(onPressed: (){
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                PaginaImoveis(_busca)));
+                  }, child: Text("Procurar no Google Maps"))
+                ],
               ),
-            )
+            ),
+
           ],
         ),
       ),
