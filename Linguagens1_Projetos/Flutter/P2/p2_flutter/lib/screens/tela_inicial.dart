33e40eda364ce75f2148b74f7bba5d12dbaad785
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-11-29 00:36:25 -0300

    Implementação do botao de buscar
    Botao que irá buscar na api os imoveis relacionados os campos da busca

--- Linguagens1_Projetos/Flutter/P2/p2_flutter/lib/screens/tela_inicial.dart
+++ Linguagens1_Projetos/Flutter/P2/p2_flutter/lib/screens/tela_inicial.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:p2_flutter/models/imoveis_json.dart';
 import 'package:p2_flutter/screens/pagina_imoveis.dart';
+import 'package:p2_flutter/utilities/network_helper.dart';
 
 class TelaInicial extends StatelessWidget {
 
@@ -9,29 +11,37 @@ class TelaInicial extends StatelessWidget {
   final controladorValorMax = TextEditingController();
   final controladorDorms = TextEditingController();
 
+  var _busca = [];
+
   final controlador = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Padding(
-          padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(8.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SizedBox(width:400, height: 200, child: Image.asset("assets/Logo/LogoImovel.png")),
-              Text("Procure seu imovel!",
+              Text("Procure seu imovel no ABC paulista!",
                   style: TextStyle(fontSize: 20.0)),
               personalTextField(controladorCidade, "Cidade de interesse","Cidade" , Icon(Icons.location_city)),
               personalTextField(controladorBairro, "Bairro de interesse","Bairro", Icon(Icons.park)),
               personalTextField(controladorValorMax, "Valor máximo de interesse", "Valor até", Icon(Icons.monetization_on_outlined)),
               personalTextField(controladorDorms, "Quantidade de dormitórios", "Dormitórios", Icon(Icons.home)),
-              FlatButton(onPressed: (){
-                buscar_caracteristicas_imovel();
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (BuildContext context) => PaginaImoveis()));
-              }, child: Text("Buscar",
+              FlatButton(onPressed: () async{
+                await buscar_caracteristicas_imovel();
+                print(_busca);
+                if(_busca.length != 0) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  PaginaImoveis(_busca)));
+                    }
+                  }, child: Text("Buscar",
                 style: TextStyle(fontSize: 20.0),
               ))
             ],
@@ -52,10 +62,49 @@ class TelaInicial extends StatelessWidget {
     );
   }
 
-  void buscar_caracteristicas_imovel() {
-    print(controladorCidade.text);
-    print(controladorBairro.text);
-    print(controladorValorMax.text);
-    print(controladorDorms.text);
+  Future<void> buscar_caracteristicas_imovel()async{
+    var _requisicao = NetworkHelper(url: "https://api.estagio.amfernandes.com.br/imoveis");
+    var _dados = [];
+
+    for (var entrada in await _requisicao.getData()){
+      try {
+        _dados.add(Imovel.fromJson(entrada));
+      } catch(e) {
+      }
+    }
+
+    for ( var entrada in _dados){
+      Imovel imovelAtual = entrada;
+
+      if(imovelAtual.cidade == controladorCidade.text && controladorCidade.text != ""){
+        _busca.add(imovelAtual);
+      }
+      if(imovelAtual.bairro != controladorBairro.text && controladorBairro.text != ""){
+        _busca.remove(imovelAtual);
+      }
+
+      try{
+        if(imovelAtual.planta.preco > double.parse(controladorValorMax.text) && controladorBairro.text != ""){
+          _busca.remove(imovelAtual);
+        }
+      }catch(e){
+        if(imovelAtual.bairro != controladorBairro.text && controladorBairro.text != ""){
+          _busca.remove(imovelAtual);
+        }
+      }
+
+      try{
+        if(imovelAtual.planta.dorms > double.parse(controladorDorms.text) && controladorBairro.text != ""){
+          _busca.remove(imovelAtual);
+        }
+      }catch(e){}
+    }
+
+    print(_dados.length);
+    print(_busca.length);
+
   }
 }
+
+
+
