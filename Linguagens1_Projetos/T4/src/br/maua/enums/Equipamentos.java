dc09bd63f6683dcd46da564764259b7299402756
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-12-03 06:41:54 -0300

    Criação dos enums de equipamento
    Implementação dos métodos de getter e reconhecimento para DAO.
    Os valores que estão nos enums é referente a posição em que ele se encontra no personagem, exemplo: 1 = cabeça, 2 = tronco.

--- Linguagens1_Projetos/T4/src/br/maua/enums/Equipamentos.java
+++ Linguagens1_Projetos/T4/src/br/maua/enums/Equipamentos.java
@@ -1,4 +1,36 @@
 package br.maua.enums;
 
 public enum Equipamentos {
+    NONE(0),
+    ELMO_DE_COURO(1),
+    PEITORAL_DE_COURO(2),
+    CALCAS_DE_COURO(3),
+    BOTAS_DE_COURO(4),
+    ELMO_DE_FERRO(1),
+    PEITORAL_DE_FERRO(2),
+    CALCAS_DE_FERRO(3),
+    BOTAS_DE_FERRO(4),
+    ELMO_DE_PLATINA(1),
+    PEITORAL_DE_PLATINA(2),
+    CALCAS_DE_PLATINA(3),
+    BOTAS_DE_PLATINA(4);
+
+    private int valor;
+
+    Equipamentos(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public static Equipamentos getEquipamento(String equipamento){
+        for(Equipamentos equip:Equipamentos.values()){
+            if(equip.toString().equals(equipamento.toUpperCase())){
+                return equip;
+            }
+        }
+        return null;
+    }
 }
