62ee63744fdbc188d29585ac1e5722932899a1fb
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-12-03 00:50:45 -0300

    Implementação do método atualizaPersonagem

--- Linguagens1_Projetos/T4/src/br/maua/aplicacoes/AplicacaoDAO.java
+++ Linguagens1_Projetos/T4/src/br/maua/aplicacoes/AplicacaoDAO.java
@@ -25,12 +25,15 @@ public class AplicacaoDAO {
                     alive = false;
                     break;
                 case 1:
-                    System.out.print("Personagens: ");
                     exibirPersonagens();
                     break;
                 case 2:
                     System.out.print("Criação de personagem: ");
                     cadastrarPersonagem();
+                    break;
+                case 3:
+                    System.out.print("Atualização do personagem: ");
+                    atualizaPersonagem();
                     break;
                 default:
                     System.out.println("Opcao Invalida!");
@@ -40,46 +43,35 @@ public class AplicacaoDAO {
     }
 
     private void menu() {
-        System.out.println("Data Base Animes e Mangas");
+        System.out.println("Data Base Personagens");
 
         System.out.println("1 - Consultar personagens");
         System.out.println("2 - Cadastra personagem");
+        System.out.println("3 - Alterar personagem");
         System.out.println("0 - Sair");
     }
 
     private void exibirPersonagens() {
         personagemList = personagemDAO.getAll();
-        System.out.println("Animes:");
+        System.out.println("Personagens:");
         personagemList.forEach( personagem -> System.out.println(personagem));
     }
 
     private void cadastrarPersonagem(){
         try {
             System.out.println("Informe o ");
-            System.out.println("nome:");
-            String nome = scanner.next();
-            System.out.println("raça:");
-            Raca raca = escolhaRaca();
-            System.out.println("profissao:");
-            Profissao profissao = escolhaProfissao();
-            System.out.println("mana:");
-            Integer mana = scanner.nextInt();
-            System.out.println("ad:");
-            Integer ad = scanner.nextInt();
-            System.out.println("ap:");
-            Integer ap = scanner.nextInt();
-            System.out.println("def:");
-            Integer def = scanner.nextInt();
-            System.out.println("defM:");
-            Integer defM = scanner.nextInt();
-            System.out.println("velocidade:");
-            Integer velocidade = scanner.nextInt();
-            System.out.println("destreza:");
-            Integer destreza = scanner.nextInt();
-            System.out.println("experiencia:");
-            Integer experiencia = scanner.nextInt();
-            System.out.println("nivel:");
-            Integer nivel = scanner.nextInt();
+            System.out.println("nome:");String nome = scanner.next();
+            System.out.println("raça:");Raca raca = escolhaRaca();
+            System.out.println("profissao:");Profissao profissao = escolhaProfissao();
+            System.out.println("mana:");Integer mana = scanner.nextInt();
+            System.out.println("ad:");Integer ad = scanner.nextInt();
+            System.out.println("ap:");Integer ap = scanner.nextInt();
+            System.out.println("def:");Integer def = scanner.nextInt();
+            System.out.println("defM:");Integer defM = scanner.nextInt();
+            System.out.println("velocidade:");Integer velocidade = scanner.nextInt();
+            System.out.println("destreza:");Integer destreza = scanner.nextInt();
+            System.out.println("experiencia:");Integer experiencia = scanner.nextInt();
+            System.out.println("nivel:");Integer nivel = scanner.nextInt();
 
             Personagem personagem = new Personagem(
                     nome, raca, profissao, mana, ad, ap, def, defM, velocidade, destreza, experiencia, nivel
@@ -139,5 +131,106 @@ public class AplicacaoDAO {
         System.out.println("Voce escolheu a profissao: " + profissaoEscolhida.getValor());
         return profissaoEscolhida;
 
+    }
+
+    private void atualizaPersonagem(){
+        System.out.println("Informe o nome do personagem que queira alterar.");
+
+        personagemList = personagemDAO.getAll();
+        System.out.println("Seus personagens são: ");
+        personagemList.forEach( personagem -> System.out.println(personagem.getNome()));
+
+        String nomeEscolhido = scanner.next();
+
+        for (Personagem personagem : personagemList) {
+            System.out.print(nomeEscolhido + " ->");
+            System.out.println(personagem.getNome());
+            if(nomeEscolhido.equals(personagem.getNome())){
+                System.out.println(personagem);
+                String nome = personagem.getNome();
+                Raca raca = personagem.getRaca();
+                Profissao profissao = personagem.getProfissao();
+                Integer mana = personagem.getMana();
+                Integer ad = personagem.getAd();
+                Integer ap = personagem.getAp();
+                Integer def = personagem.getDef();
+                Integer defM = personagem.getDefM();
+                Integer velocidade = personagem.getVelocidade();
+                Integer destreza = personagem.getDestreza();
+                Integer experiencia = personagem.getExperiencia();
+                Integer nivel = personagem.getNivel();
+
+                boolean alive = true;
+                do{
+                    menuUpdatePersonagem();
+                    int opcao = Integer.parseInt(scanner.next());
+                    switch (opcao){
+                        case 0:
+                            alive = false;
+                            break;
+                        case 1:
+                            profissao = escolhaProfissao();
+                            break;
+                        case 2:
+                            mana = escolhaProprietadeInteger("da Mana");
+                            break;
+                        case 3:
+                            ad = escolhaProprietadeInteger("do Ad");
+                            break;
+                        case 4:
+                            ap = escolhaProprietadeInteger("do Ap");
+                            break;
+                        case 5:
+                            def = escolhaProprietadeInteger("da Def");
+                            break;
+                        case 6:
+                            defM = escolhaProprietadeInteger("da DefM");
+                            break;
+                        case 7:
+                            velocidade = escolhaProprietadeInteger("da Velocidade");
+                            break;
+                        case 8:
+                            destreza = escolhaProprietadeInteger("da Destreza");
+                            break;
+                        case 9:
+                            experiencia = escolhaProprietadeInteger("da Experiência");
+                            break;
+                        case 10:
+                            nivel = escolhaProprietadeInteger("do Nivel");
+                            break;
+                        default:
+                            System.out.println("Opcao Invalida!");
+                    }
+
+                }while(alive);
+
+                personagemDAO.update( new Personagem(
+                        nome, raca, profissao, mana, ad, ap, def, defM, velocidade, destreza, experiencia, nivel
+                ));
+                break;
+            }
+        }
+    }
+
+    private Integer escolhaProprietadeInteger(String propriedade) {
+        System.out.print("Digite o valor " + propriedade);
+        Integer escolha = scanner.nextInt();
+        return escolha;
+    }
+
+    private void menuUpdatePersonagem() {
+        System.out.println("Escolha a propriedade a ser mudada");
+
+        System.out.println("1 - Profissao");
+        System.out.println("2 - Mana");
+        System.out.println("3 - Ad");
+        System.out.println("4 - Ap");
+        System.out.println("5 - Def");
+        System.out.println("6 - DefM");
+        System.out.println("7 - Velocidade");
+        System.out.println("8 - Destreza");
+        System.out.println("9 - Experiencia");
+        System.out.println("10 - Nivel");
+        System.out.println("0 - Sair");
     }
 }
