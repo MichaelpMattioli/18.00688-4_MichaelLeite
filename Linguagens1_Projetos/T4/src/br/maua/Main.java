b8e64047b39a97a51b4815e76799a6f049409315
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-12-02 21:27:14 -0300

    Teste da main da aplicacaoDAO

--- Linguagens1_Projetos/T4/src/br/maua/Main.java
+++ Linguagens1_Projetos/T4/src/br/maua/Main.java
@@ -1,8 +1,11 @@
 package br.maua;
 
+import br.maua.aplicacoes.AplicacaoDAO;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        AplicacaoDAO aplicacaoDAO = new AplicacaoDAO();
+        aplicacaoDAO.run();
     }
 }
