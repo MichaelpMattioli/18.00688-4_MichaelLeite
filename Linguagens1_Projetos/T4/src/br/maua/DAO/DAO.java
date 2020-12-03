fd80834de353e1d28eb87587bc2ef780b9736e9e
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-12-03 00:50:21 -0300

    Implementação dos métodos abstratos da interface DAO

--- Linguagens1_Projetos/T4/src/br/maua/DAO/DAO.java
+++ Linguagens1_Projetos/T4/src/br/maua/DAO/DAO.java
@@ -1,10 +1,13 @@
 package br.maua.DAO;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface DAO <T> {
     List<T> get(String condition);
     List<T> getAll();
-    void create(T t);
+    void update(T t) throws SQLException;
+    void delete(T t);
+    void create(T t) throws SQLException;
 }
 
