96277ea2cba6f78ca2c355e30d54ab643272d80a
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-12-03 00:49:41 -0300

    Implementação dos métodos abstratos de update DAO

--- Linguagens1_Projetos/T4/src/br/maua/DAO/DAOFields.java
+++ Linguagens1_Projetos/T4/src/br/maua/DAO/DAOFields.java
@@ -2,10 +2,9 @@ package br.maua.DAO;
 
 public interface DAOFields {
     String getTableName();
-
+    String getDeleteString(String table);
+    String getUpdateString(String table);
     String getInsertString(String table);
-
     String getSelectAllString(String table);
-
     String getSelectConditionalString(String table);
 }
