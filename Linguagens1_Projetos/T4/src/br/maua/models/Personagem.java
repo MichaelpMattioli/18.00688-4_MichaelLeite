5728a73239d51920d24674227709e6ea7a3adeac
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-12-03 06:39:52 -0300

    Refatoração classe Personagem, Implementação de equipamentos
    Adição de variaveis que compoem a armadura do personagem

--- Linguagens1_Projetos/T4/src/br/maua/models/Personagem.java
+++ Linguagens1_Projetos/T4/src/br/maua/models/Personagem.java
@@ -1,7 +1,11 @@
 package br.maua.models;
 
+import br.maua.enums.Equipamentos;
+import br.maua.enums.Itens;
 import br.maua.enums.Profissao;
 import br.maua.enums.Raca;
+
+import java.util.HashMap;
 
 
 public class Personagem {
@@ -17,6 +21,29 @@ public class Personagem {
     private Integer destreza;
     private Integer experiencia;
     private Integer nivel;
+    private Equipamentos cabeca;
+    private Equipamentos tronco;
+    private Equipamentos perna;
+    private Equipamentos pes;
+
+    public Personagem(String nome, Raca raca, Profissao profissao, Integer mana, Integer ad, Integer ap, Integer def, Integer defM, Integer velocidade, Integer destreza, Integer experiencia, Integer nivel, Equipamentos cabeca, Equipamentos tronco, Equipamentos perna, Equipamentos pes) {
+        this.nome = nome;
+        this.raca = raca;
+        this.profissao = profissao;
+        this.mana = mana;
+        this.ad = ad;
+        this.ap = ap;
+        this.def = def;
+        this.defM = defM;
+        this.velocidade = velocidade;
+        this.destreza = destreza;
+        this.experiencia = experiencia;
+        this.nivel = nivel;
+        this.cabeca = cabeca;
+        this.tronco = tronco;
+        this.perna = perna;
+        this.pes = pes;
+    }
 
     public Personagem(String nome, Raca raca, Profissao profissao, Integer mana, Integer ad, Integer ap, Integer def, Integer defM, Integer velocidade, Integer destreza, Integer experiencia, Integer nivel) {
         this.nome = nome;
@@ -31,6 +58,10 @@ public class Personagem {
         this.destreza = destreza;
         this.experiencia = experiencia;
         this.nivel = nivel;
+        this.cabeca = Equipamentos.NONE;
+        this.tronco = Equipamentos.NONE;
+        this.perna = Equipamentos.NONE;
+        this.pes = Equipamentos.NONE;
     }
 
     public String getNome() {
@@ -81,6 +112,21 @@ public class Personagem {
         return nivel;
     }
 
+    public Equipamentos getCabeca() {
+        return cabeca;
+    }
+
+    public Equipamentos getTronco() {
+        return tronco;
+    }
+
+    public Equipamentos getPerna() {
+        return perna;
+    }
+
+    public Equipamentos getPes() {
+        return pes;
+    }
 
     @Override
     public String toString() {
@@ -97,6 +143,12 @@ public class Personagem {
                 ", destreza=" + destreza +
                 ", experiencia=" + experiencia +
                 ", nivel=" + nivel +
+                ", armadura{"+
+                    "cabeca=" + cabeca +
+                    ", tronco=" + tronco +
+                    ", perna=" + perna +
+                    ", pes=" + pes +
+                "}" +
                 '}';
     }
 }
